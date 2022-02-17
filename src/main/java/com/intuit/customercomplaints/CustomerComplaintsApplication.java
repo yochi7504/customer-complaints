8886cf9921df91b3a15766e3dff563e7f95ab0fc
yochi7504 <yochi7504@gmail.com>
2022-02-17 09:15:51 +0200

    Base application

---
+++ src/main/java/com/intuit/customercomplaints/CustomerComplaintsApplication.java
@@ -0,0 +1,13 @@
+package com.intuit.customercomplaints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerComplaintsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerComplaintsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intuit/customercomplaints/CustomerComplaintsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intuit.customercomplaints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerComplaintsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
